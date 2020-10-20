0dbb190f4e8ed5095cfd35f0674403c3da2c6226
Samuel Rasquinha <samuelrasquinha@gmail.com>
2020-10-21 01:46:10 +0530

    Added buggy texturing

--- Source/Core/Shaders/ReflectionFrag.glsl
+++ Source/Core/Shaders/ReflectionFrag.glsl
@@ -10,7 +10,7 @@ uniform int u_HasAlbedoMap = 1;
 
 void main()
 {
-	/*if (u_HasAlbedoMap == 1)
+	if (u_HasAlbedoMap == 1)
 	{
 		o_Color = texture(u_AlbedoMap, v_TexCoords);
 	}
@@ -18,8 +18,5 @@ void main()
 	else
 	{
 		o_Color = vec4(u_DefaultColor, 1.0f);
-	}*/
-
-	o_Color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
-	
+	}
 }
